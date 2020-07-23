40c9e2a8b9fe0e10a20b48b6c1594648d2c6b5ab
Sarathkrishnan <sarathkrishnangs@gmail.com>
2020-07-23 23:30:20 +0530

    progress bar added. Implimented Using

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+//import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:animated_splash/animated_splash.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:async';
+import 'package:progress_indicators/progress_indicators.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -13,19 +16,56 @@ void main() {
   ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState('https://vaisnaviagarbati.com');
+}
+
+class _MyAppState extends State<MyApp> {
+  var _url = 'https://vaisnaviagarbati.com';
+  final _key = UniqueKey();
+
+  _MyAppState(this._url);
+  num _stackToView = 1;
+
+  void _handleLoad(String value) {
+    setState(() {
+      _stackToView = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Vaisnavi Agarbaties",
-      routes: {
-        "/": (_) => new SafeArea(
-              child: WebviewScaffold(
-                url: "https://vaisnaviagarbati.com",
-              ),
-            )
-      },
-    );
+    return Scaffold(
+        body: SafeArea(
+            child: IndexedStack(
+      index: _stackToView,
+      children: [
+        Column(
+          children: <Widget>[
+            Expanded(
+                child: WebView(
+              key: _key,
+              javascriptMode: JavascriptMode.unrestricted,
+              initialUrl: _url,
+              onPageFinished: _handleLoad,
+            )),
+          ],
+        ),
+        Container(
+          color: Colors.white,
+          child: Center(
+            child: CircularProgressIndicator(),
+          ),
+        ),
+      ],
+    )));
   }
+}
+
+class WebViewContainer extends StatefulWidget {
+  final url;
+  WebViewContainer(this.url);
+  @override
+  createState() => _MyAppState(this.url);
 }
